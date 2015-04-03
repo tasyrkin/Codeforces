4329f53aad8c06c30e639edbf0cee9f066e9a606
Timofey Asyrkin <tasyrkin@gmail.com>
2015-04-03 23:00:30 +0200

    Solved first task (CF297A) with Scala

--- src/main/scala/CF297A.scala
+++ src/main/scala/CF297A.scala
@@ -1,9 +1,55 @@
-import scala.collection.mutable
-import scala.io.{StdIn, Source}
-
-class CF297A extends App {
+object CF297A extends App {
+  import scala.collection.mutable.HashMap
+  import scala.io.{StdIn}
+  
   val n: Int = StdIn.readInt()
   val keysAndRooms: String = StdIn.readLine()
-  //keysAndRooms.toIterable.
-  print ("Hello World")
+  var acc = 0
+  
+  keysAndRooms.toIterable.foreach(solve(new Multiset[Char]))
+  
+  print (acc)
+
+  def solve(multiset: Multiset[Char]): (Char) => Unit = {
+    (ch: Char) => {
+      if(ch.isLower){
+        multiset.add(ch.toUpper)
+      } else {
+        if(multiset.contains(ch)){
+          multiset.remove(ch)
+        } else{
+          acc = acc + 1
+        }
+      }
+    }
+  }
+
+
+  /**
+   * Duplication of lib.Multiset because the whole code must be in one file
+   * @tparam T a type variable stored in the multiset
+   */
+  class Multiset[T] {
+    val hashMap = new HashMap[T, Int]()
+
+    def add(element: T) = {
+      val count = hashMap.getOrElseUpdate(element, 0)
+      hashMap+=((element, count+1))
+    }
+
+    def contains(element:T ) = {
+      hashMap.get(element).nonEmpty
+    }
+
+    def remove(element: T) = {
+      val count = hashMap.getOrElse(element, 1)
+      if (count == 1) {
+        hashMap.remove(element)
+      } else {
+        hashMap+=((element, count-1))
+      }
+    }
+  }
+
 }
+
--- src/test/scala/lib/MultisetSpec.scala
+++ src/test/scala/lib/MultisetSpec.scala
@@ -10,4 +10,14 @@ class MultisetSpec extends FlatSpec with Matchers {
     multiset.contains(1) should be (true)
     multiset.contains(2) should be (true)
   }
+  
+  "A multiset" should "not contain values removed from it" in {
+    val multiset = new Multiset[Int]
+    multiset.add(1)
+    multiset.add(2)
+    multiset.remove(1)
+    multiset.remove(2)
+    multiset.contains(1) should be (false)
+    multiset.contains(2) should be (false)
+  }
 }
