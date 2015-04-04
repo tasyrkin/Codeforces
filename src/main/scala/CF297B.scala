b918e2b676ba61c48fbd90d5dee108abb42f04d1
Timofey Asyrkin <tasyrkin@gmail.com>
2015-04-04 21:37:37 +0200

    Better implementation of CF297B

--- src/main/scala/CF297B.scala
+++ src/main/scala/CF297B.scala
@@ -5,40 +5,23 @@ object CF297B extends App{
   val input = StdIn.readLine().toCharArray
   val inputLength = input.length
   val n: Int = StdIn.readInt()
-  val inputArray = StdIn.readLine().split("\\s+").map(x => x.toInt-1).sorted.toList :+ inputLength / 2
+  val inputArray = StdIn.readLine().split("\\s+").map(x => x.toInt-1)
 
-//  val hashMap = new mutable.HashMap[Int, Int]()
-//
-//  inputArray.foreach(x => {
-//    val value = hashMap.get(x)
-//    var cnt = 1
-//    if(value.isEmpty){
-//      hashMap.put(x, 1)
-//    } else {
-//      hashMap.put(x, value.get + 1)
-//    }
-//  })
-//
-//  val sorted = hashMap.filter(keyValue => keyValue._2 % 2 == 1).keys.toList.sorted :+ inputLength / 2
-  
-  val sorted = mutable.MutableList[Int]()
-  var prev = inputArray.head
-  var cnt = 0 
-  inputArray.foreach(value => {
-    if (prev != value){
-      if(cnt % 2 == 1){
-        sorted += prev
-      }
-      cnt = 1
-      prev = value
+  val hashMap = new mutable.HashMap[Int, Int]()
+  hashMap.put(inputLength / 2, 1)
+
+  inputArray.foreach(x => {
+    val value = hashMap.get(x)
+    var cnt = 1
+    if(value.isEmpty){
+      hashMap.put(x, 1)
     } else {
-      cnt = cnt + 1
+      hashMap.put(x, value.get + 1)
     }
   })
-  if(cnt % 2 == 1)sorted += prev
   
-  val arr = sorted.toArray
-  
+  val arr = hashMap.filter(keyValue => keyValue._2 % 2 == 1).keys.toArray.sorted
+
   for((value1,index) <- arr.view.zipWithIndex){
     if(index % 2 == 0 && index < arr.length-1){
       for(indexToChange <- value1 until arr(index+1)){
